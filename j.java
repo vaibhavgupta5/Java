213c874b7f0f60a65920ca9268021ce5d2eaf8fe
vaibhavgupta5 <vaibhavgupta.v890@gmail.com>
2025-08-25 11:18:24 +0530

    first commit

---
+++ j.java
@@ -0,0 +1,15 @@
+public class j {
+    public static void main(String[] args) {
+        System.err.println("hi");
+        if (args.length > 0) {
+            System.err.println("Argument: " + args[0]);
+        }
+    }
+
+}
+
+class aa {
+    public static void main(String[] args) {
+        
+    }
+}
